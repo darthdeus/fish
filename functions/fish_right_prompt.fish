02ea3fe299b1fd8fb84812997a69331854c57786
Jakub Arnold <darthdeus@gmail.com>
2012-11-30 14:41:09 +0100

    Add right prompt with branch@rev

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,3 +1,20 @@
+function __current_branch
+  echo -n (git symbolic-ref HEAD 2>/dev/null | sed "s|refs/heads/||")
+end
+
+function __current_commit
+  echo -n (git rev-parse --short HEAD 2>/dev/null)	
+end
+
+
 function fish_right_prompt
-  echo (git rev-parse --short HEAD 2>/dev/null)	
+
+  set_color blue
+  __current_branch
+  echo -n "@"
+
+  set_color yellow
+  __current_commit
+
+  set_color normal
 end
