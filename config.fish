9679aa7d231ae2131eb0d8eb55c18977192f48fa
Jakub Arnold <darthdeus@gmail.com>
2012-12-02 09:59:36 +0100

    Set default editor as VIM

--- config.fish
+++ config.fish
@@ -2,4 +2,7 @@ set -x PATH /usr/local/bin /usr/local/sbin $PATH
 
 set -x PATH /usr/local/opt/rbenv/bin $PATH
 set -x PATH $HOME/.rbenv/shims $PATH
+
+set -xg EDITOR vim
+
 rbenv rehash >/dev/null ^&1
