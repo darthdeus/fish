437f40994da102ca55e41f349065d12948d68954
Jakub Arnold <darthdeus@gmail.com>
2012-12-02 14:17:59 +0100

    Make `rake` zeus aware

--- functions/rake.fish
+++ functions/rake.fish
@@ -1,7 +1,15 @@
-function rake
+function __bundle_aware_rake
   if test -f Rakefile
     bundle exec rake $argv
   else
     ruby -S rake $argv
+  end
+end
+
+function rake
+  if test -S .zeus.sock
+    zeus rake $argv
+  else
+    __bundle_aware_rake $argv
   end
 end
