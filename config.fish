df6f8187fc44e2682ccbe685e026460c7e155897
Jakub Arnold <darthdeus@gmail.com>
2012-12-04 15:06:05 +0100

    Add gch alias

--- config.fish
+++ config.fish
@@ -7,28 +7,29 @@ set -xg EDITOR vim
 
 rbenv rehash >/dev/null ^&1
 
-function g    ; git $argv           ; end
-function s    ; git status -sb      ; end
-function d    ; git diff $argv      ; end
-function gd   ; d $argv             ; end
-function gdc  ; d --cached          ; end
-function gf   ; git fetch $argv     ; end
-function gst  ; git stash $argv     ; end
-function grb  ; git rebase $argv    ; end
-function gco  ; git checkout $argv  ; end
-function gb   ; git branch $argv    ; end
-function gc   ; git commit $argv    ; end
-function ga   ; git add $argv       ; end
-function gau  ; git add -u          ; end
-function gr   ; git reset           ; end
-function grh  ; git reset --hard    ; end
-function grv  ; git remote -v       ; end
-function gp   ; git push $argv      ; end
-function gup  ; git smart-pull      ; end
-function gap  ; git add -p          ; end
-function gcam ; git commit --amend  ; end
-function gm   ; git merge --no-ff   ; end
-function gmf  ; git merge --ff-only ; end
+function g    ; git $argv             ; end
+function s    ; git status -sb        ; end
+function d    ; git diff $argv        ; end
+function gd   ; d $argv               ; end
+function gdc  ; d --cached            ; end
+function gf   ; git fetch $argv       ; end
+function gst  ; git stash $argv       ; end
+function grb  ; git rebase $argv      ; end
+function gco  ; git checkout $argv    ; end
+function gb   ; git branch $argv      ; end
+function gc   ; git commit $argv      ; end
+function ga   ; git add $argv         ; end
+function gau  ; git add -u            ; end
+function gr   ; git reset             ; end
+function grh  ; git reset --hard      ; end
+function grv  ; git remote -v         ; end
+function gp   ; git push $argv        ; end
+function gup  ; git smart-pull        ; end
+function gap  ; git add -p            ; end
+function gcam ; git commit --amend    ; end
+function gm   ; git merge --no-ff     ; end
+function gmf  ; git merge --ff-only   ; end
+function gch  ; git cherry-pick $argv ; end
 
 # log
 function gl   ; glog --graph $argv ; end
